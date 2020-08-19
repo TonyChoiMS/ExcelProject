0e15706c1336465a7fc1fb8af9fc0da95f632be4
TonyChoi <cms9286@gmail.com>
2020-08-20 00:39:06 +0900

    add Utility class
    define stack, vector class

--- ExcelProject/Utils.h
+++ ExcelProject/Utils.h
@@ -1,6 +1,75 @@
 #pragma once
-class Utils
+
+#ifndef UTILS_H
+#define UTILS_H
+
+
+#include <string>
+using std::string;
+
+namespace MyExcel
 {
+	class Vector
+	{
+		string* data;
+		int capacity;
+		int length;
 
-};
+	public:
+		Vector(int n = 1);
+		~Vector();
 
+		void pushBack(string s);
+		string operator[](int i);
+		void remove(int x);
+		int size();
+	};
+
+	class Stack
+	{
+		struct Node
+		{
+			Node* prev;
+			string s;
+
+			Node(Node* prev, string s) : prev(prev), s(s) {}
+		};
+
+		Node* current;
+		Node start;
+
+	public:
+		Stack();
+		~Stack();
+
+		void push(string s);
+		string pop();
+		string peek();
+		bool isEmpty();
+	};
+
+	class NumStack
+	{
+		struct Node
+		{
+			Node* prev;
+			double s;
+
+			Node(Node* prev, double s) : prev(prev), s(s) {}
+		};
+
+		Node* current;
+		Node start;
+
+	public:
+		NumStack();
+		~NumStack();
+
+		void push(double s);
+		double pop();
+		double peek();
+		bool isEmpty();
+	};
+}
+
+#endif
