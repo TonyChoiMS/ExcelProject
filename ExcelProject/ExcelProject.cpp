3a011e7ea8a561d2ffbf56e9299f3517f67e22cb
TonyChoiMS <cms9286@gmail.com>
2020-08-22 17:18:04 +0900

    edit excel class
    using templete vector class

--- ExcelProject/ExcelProject.cpp
+++ ExcelProject/ExcelProject.cpp
@@ -10,14 +10,15 @@ using std::string;
 
 namespace MyExcel
 {
+	templete <typename T>
 	class Vector
 	{
-		string* data;
+		T* data;
 		int capacity;
 		int length;
 
 	public:
-		Vector(int n = 1);
+		Vector(int n = 1) : data(new T[n]), capacity(n), length(0) {}
 		~Vector();
 
 		void pushBack(string s);
@@ -80,16 +81,65 @@ namespace MyExcel
 		int x, y;
 		Table* table;
 
+	public:
+		Cell(int x, int y, Table* table);
+		~Cell();
+
+		virtual string stringify() = 0;
+		virtual int toNumeric() = 0;
+	};
+
+	class StringCell : public Cell
+	{
 		string data;
 
 	public:
-		Cell(string data, int x, int y, Table* table);
-		~Cell();
+		string stringify() override;
+		int toNumeric() override;
 
-		virtual string stringify();
-		virtual int toNumeric();
+		StringCell(string data, int x, int y, Table* t);
+	};
 
+	class NumberCell : public Cell
+	{
+		int data;
+		
+	public:
+		string stringify() override;
+		int toNumeric() override;
 
+		NumberCell(int data, int x, int y, Table* t);
+	};
+
+	class DateCell : public Cell
+	{
+		time_t data;
+
+	public:
+		string stringify() override;
+		int toNumeric() override;
+
+		DateCell(string s, int x, int y, Table* t);
+	};
+
+	class ExprCell : public Cell
+	{
+		string data;
+		string* parsed_expr;
+
+		Vector exp_vec;
+		
+		// 연산자 우선 순위를 반환합니다.
+		int precedence(char c);
+
+		// 수식을 분석합니다.
+		void parse_expression();
+
+	public:
+		ExprCell(string data, int x, int y, Table* t);
+
+		string stringify() override;
+		int toNumeric() override;
 	};
 
 	class Table
@@ -152,19 +202,17 @@ namespace MyExcel
 	};
 
 	// Vector~~~~
-	Vector::Vector(int n) : data(new string[n]), capacity(n), length(0) {}
-
 	Vector::~Vector()
 	{
 		if (data)
 			delete[] data;
 	}
 
-	void Vector::pushBack(string s)
+	void Vector::pushBack(T s)
 	{
 		if (capacity <= length)
 		{
-			string* temp = new string[capacity * 2];
+			T* temp = new T[capacity * 2];
 			for (int i = 0; i < length; i++)
 			{
 				temp[i] = data[i];
@@ -179,7 +227,7 @@ namespace MyExcel
 		length++;
 	}
 
-	string Vector::operator[](int i)
+	T Vector::operator[](int i)
 	{
 		return data[i];
 	}
@@ -193,10 +241,7 @@ namespace MyExcel
 		length--;
 	}
 
-	int Vector::size()
-	{
-		return length;
-	}
+	int Vector::size() { return length; }
 
 	// Stack~~~~
 	Stack::Stack() : start(NULL, "") { current = &start; }
@@ -282,11 +327,166 @@ namespace MyExcel
 	}
 
 	// Cell~~~~
-	Cell::Cell(string data, int x, int y, Table* table) : data(data), x(x), y(y), table(table) {}
+	Cell::Cell(int x, int y, Table* table) : x(x), y(y), table(table) {}
 
-	string Cell::stringify() { return data; }
+	// StringCell~~~~
+	StringCell::StringCell(string data, int x, int y, Table* t) : data(data), Cell(x, y, t){}
+	string StringCell::stringify() { return data; }
+	int StringCell::toNumeric() { return 0; }
+	/////
 
-	int Cell::toNumeric() { return 0; }
+	// NumberCell
+	NumberCell::NumberCell(int data, int x, int y, Table* t) : data(data), Cell(x, y, t) {}
+	string NumberCell::stringify() { return std::to_string(data); }
+	int NumberCell::toNumeric() { return data; }
+	/////
+
+	// DateCell
+	string DateCell::stringify() 
+	{
+		char buf[50];
+		tm temp;
+		localtime_s(&temp, &data);
+
+		return string(buf);
+	}
+
+	int DateCell::toNumeric() { return static_cast<int>(data); }
+
+	DateCell::DateCell(string s, int x, int y, Table* t) : Cell(x, y, t)
+	{
+		//  입력받는 Date 형식은 항상 yyyy-mm-dd  꼴이라 가정한다.
+		int year = atoi(s.c_str());
+		int month = atoi(s.c_str() + 5);
+		int day = atoi(s.c_str() + 8);
+
+		tm timeinfo;
+
+		timeinfo.tm_year = year - 1900;
+		timeinfo.tm_mon = month - 1;
+		timeinfo.tm_mday = day;
+		timeinfo.tm_hour = 0;
+		timeinfo.tm_min = 0;
+		timeinfo.tm_sec = 0;
+
+		data = mktime(&timeinfo);
+	}
+	/////
+
+	// ExprCell
+	int ExprCell::toNumeric()
+	{
+		double result = 0;
+		NumStack stack;
+
+		for (size_t i = 0; i < exp_vec.size(); i++)
+		{
+			string s = exp_vec[i];
+
+			// 셀일 경우
+			if (isalpha(s[0]))
+			{
+				stack.push(table->toNumeric(s));
+			}
+			else if (isdigit(s[0]))
+			{
+				stack.push(atoi(s.c_str()));
+			}
+			else
+			{
+				double y = stack.pop();
+				double x = stack.pop();
+
+				switch (s[0])
+				{
+				case '+':
+					stack.push(x + y);
+					break;
+				case '-':
+					stack.push(x - y);
+					break;
+				case '*':
+					stack.push(x * y);
+					break;
+				case '/':
+					stack.push(x / y);
+					break;
+				
+				}
+			}
+		}
+
+		return stack.pop();
+	}
+
+	int ExprCell::precedence(char c)
+	{
+		switch (c)
+		{
+		case '(':
+		case '[':
+		case '{':
+			return 0;
+
+		case '+':
+		case '-':
+			return 1;
+
+		case '*':
+		case '/':
+			return 2;
+		}
+
+		return 0;
+	}
+
+	void ExprCell::parse_expression()
+	{
+		Stack stack;
+
+		// 마지막에 스택에 남아있는 연산자들을 모두 pop해야되기 떄문에
+		// 수식 전체를 ()로 둘러 사서 exp_vec에 남아있는 연산자들이 push되게
+		// 해줍니다.
+		data.insert(0, "(");
+		data.push_back(')');
+
+		for (size_t i = 0; i < data.length(); i++)
+		{
+			if (isalpha(data[i]))				// 셀 이름의 경우 첫번째 글자가 알파벳이다.
+			{
+				exp_vec.pushBack(data.substr(i, 2));
+				i++;
+			}
+			else if (isdigit(data[i]))			// 첫번째 글자가 숫자라면 정수 데이터
+			{
+				exp_vec.pushBack(data.substr(i, 1));
+			}
+			else if (data[i] == '(' || data[i] == '[' || data[i] == '{')
+			{
+				//parenthesis
+				stack.push(data.substr(i, 1));
+			}
+			else if (data[i] == ')' || data[i] == ']' || data[i] == '}')
+			{
+				string t = stack.pop();
+				while (t != "(" && t != "[" && t != "{")
+				{
+					exp_vec.pushBack(t);
+					t = stack.pop();
+				}
+			}
+			else if (data[i] == '+' || data[i] == '-' || data[i] == '*' || data[i] == '/')
+			{
+				while (!stack.isEmpty() && precedence(stack.peek()[0] >= precedence(data[i])))
+				{
+					exp_vec.pushBack(stack.pop());
+				}
+				stack.push(data.substr(i, 1));
+			}
+		}
+	}
+
+	////
 
 	// Table~~~~
 	Table::Table(int maxRowSize, int maxColSize) : maxRowSize(maxRowSize), maxColSize(maxColSize)
@@ -519,6 +719,116 @@ namespace MyExcel
 			}
 		}
 	}
+
+	// Class Excel
+	class Excel
+	{
+		Table* current_table;
+
+	public:
+		Excel(int maxRow, int maxCol, int choice);
+
+		int parseUserInput(string s);
+		void commandLine();
+	};
+
+	Excel::Excel(int maxRow, int maxCol, int choice = 0)
+	{
+		switch (choice) 
+		{
+			case 0:
+				current_table = new TxtTable(maxRow, maxCol);
+			break;
+			case 1:
+				current_table = new CSVTable(maxRow, maxCol);
+			break;
+			default:
+				current_table = new HtmlTable(maxRow, maxCol);
+		}
+	}
+
+	int parseUserInput(string s)
+	{
+		int next = 0;
+		string command = "";
+		for (size_t i = 0; i < s.length(); i++)
+		{
+			if (s[i] == ' ')
+			{
+				command = s.substr(0, i);
+				next = i + 1;
+				break;
+			}
+			else if (i == s.length() - 1)
+			{
+				command = s.substr(0, i + 1);
+				next = i + 1;
+				break;
+			}
+		}
+
+		string to = "";
+		for (size_t i = next; i < s.length(); i++)
+		{
+			if (s[i] = ' ' || i == s.length() - 1)
+			{
+				to = s.substr(next, i - next);
+				next = i + 1;
+				break;
+			}
+			else if (i == s.length() - 1)
+			{
+				to = s.substr(0, i + 1);
+				next = i + 1;
+				break;
+			}
+		}
+
+		// Cell 이름으로 받는다.
+		int col = to[0] - 'A';
+		int row = atoi(to.c_str() + 1) - 1;
+
+		string rest = s.substr(next);
+
+		if (command == "sets")
+		{
+			current_table->reg_cell(new StringCell(rest, row, col, current_table), row, col);
+		}
+		else if (command == "setn")
+		{
+			current_table->reg_cell(new NumberCell(atoi(rest.c_str()), row, col, current_table), row, col);
+		}
+		else if (command == "setd")
+		{
+			current_table->reg_cell(new DateCell(rest, row, col, current_table), row, col);
+		}
+		else if (command == "out")
+		{
+			ofstream out(to);
+			out << *current_table;
+			std::cout << to << "에 내용이 저장되었습니다." << std::endl;
+		}
+		else if (command == "exit")
+		{
+			return 0;
+		}
+
+		return 1;
+	}
+
+	void Excel::commandLine()
+	{
+		string s;
+		std::getline(cin, s);
+
+		while (parseUserInput(s))
+		{
+			std::cout << *current_table << std::endl << ">> ";
+			getline(cin, s);
+		}
+	}
+
+	/////
 }
 
 #endif
