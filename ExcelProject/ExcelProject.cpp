6fdf1b5d72b0b209c179b179a6a43a2e226c084e
TonyChoi <cms9286@gmail.com>
2020-08-26 13:41:40 +0900

    typedef Ratio

--- ExcelProject/ExcelProject.cpp
+++ ExcelProject/ExcelProject.cpp
@@ -252,6 +252,45 @@ int factorial(int n)
 	return n * factorial(n - 1);
 }
 
+// 최대 공약수 (유클리드 호제법)
+int gcd(int a, int b)
+{
+	if (b == 0)
+		return a;
+
+	return gcd(b, a % b);
+}
+
+// 템플릿을 사용한 최대 공약수
+template <int X, int Y>
+struct GCD
+{
+	static const int value = GCD<Y, X % Y>::value;
+};
+
+template <int X>
+struct GCD<X, 0>
+{
+	static const int value = X;
+};
+
+template <int N, int D = 1>
+struct Ratio
+{
+	typedef Ratio<N, D> type;
+	static const int num = N;
+	static const int den = D;
+};
+
+template <class R1, class R2>
+struct _Ratio_Add
+{
+	typedef Ratio<R1::num * R2::den + R2::num * R1::den, R1::den * R2::den> type;
+};
+
+template <class R1, class R2>
+struct Ratio_Add : _Ratio_Add <R1, R2>::type{};
+
 
 int main()
 {
@@ -270,4 +309,16 @@ int main()
 	std::cout << "additional result : " << three::num << std::endl;
 
 	std::cout << " 6! = 1 * 2 * 3 * 4 * 5 * 6 = " << Factorial<6>::result << std::endl;
+
+	std::cout << "gcd(36, 24) :: " << GCD<36, 24>::value << ", fucntion : " << gcd(36, 24) << std::endl;
+
+	typedef Ratio<2, 3> rat;
+	typedef Ratio<3, 2> rat2;
+	//typedef Ratio_Add<rat, rat2> rat3;			// 아래 구문과 같은 의미.
+	using rat3 = Ratio_Add<rat, rat2>;
+	
+	std::cout << rat3::num << " / " << rat3::den << std::endl;
+
+	typedef void(*func)(int, int);
+	using func = void(*)(int, int);
 }
