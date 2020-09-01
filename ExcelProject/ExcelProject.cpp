e25e43ef4b2600e343e0b5bdf999e28d0a8f029b
TonyChoi <cms9286@gmail.com>
2020-09-01 09:53:02 +0900

    commit stable sort

--- ExcelProject/ExcelProject.cpp
+++ ExcelProject/ExcelProject.cpp
@@ -593,6 +593,48 @@ void print(Iter begin, Iter end)
 	std::cout << std::endl;
 }
 
+struct User
+{
+	std::string name;
+	int age;
+
+	User(std::string name, int age) : name(name), age(age) {}
+
+	bool operator<(const User& u) const { return age < u.age; }
+};
+
+std::ostream& operator<<(std::ostream& o, const User& u)
+{
+	o << u.name << " , " << u.age;
+	return o;
+}
+
+void Test()
+{
+	std::vector<User> vec;
+	for (int i = 0; i < 100; i++)
+	{
+		std::string name = "";
+		name.push_back('a' + i / 26);
+		name.push_back('a' + i % 26);
+		vec.push_back(User(name, static_cast<int>(rand() % 10)));
+	}
+
+	std::vector<User> vec2 = vec;
+
+	std::cout << "before sort" << std::endl;
+	print(vec.begin(), vec.end());
+
+	std::sort(vec.begin(), vec.end());
+
+	std::cout << "after sort" << std::endl;
+	print(vec.begin(), vec.end());
+
+	std::cout << "stable sort" << std::endl;
+	std::stable_sort(vec2.begin(), vec2.end());
+	print(vec2.begin(), vec2.end());
+}
+
 int main()
 {
 	std::cout << StrCat(string("this"), " ", "is", " ", string("a"), " ", string("sentence"));
@@ -753,4 +795,6 @@ int main()
 
 	std::cout << "정렬 후" << std::endl;
 	print(vec.begin(), vec.end());
+
+	Test();
 }
